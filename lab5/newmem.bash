9bf1deee995e09a878c7c776e36d8910213e0d4c
zzucch <nikitamilovatskiy@gmail.com>
2024-04-08 22:57:02 +0300

    refactor: make language usage consistent

--- lab5/newmem.bash
+++ lab5/newmem.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Необходимо указать параметр N"
-    exit 1
+if [ "$1" = "" ]; then
+	echo "You must provide argument N."
+	exit 1
 fi
 
 N=$1
@@ -10,17 +10,16 @@ array=()
 step=0
 
 while true; do
-    array+=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+	array+=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 
-    ((step++))
+	((step++))
 
-    if (( ${#array[@]} > N )); then
-        echo "Размер массива превысил $N"
-        exit 0
-    fi
+	if ((${#array[@]} > N)); then
+		echo "Array size is over $N"
+		exit 0
+	fi
 
-    if ((step % 100000 == 0)); then
-        echo "Step $step: Array size is ${#array[@]}"
-    fi
+	if ((step % 100000 == 0)); then
+		echo "Step $step: Array size is ${#array[@]}"
+	fi
 done
-
