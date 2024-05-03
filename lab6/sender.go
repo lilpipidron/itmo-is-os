f173a6748d150ed2662be8ffd167bb1b0ad07d21
lilpipidron <lociX55Lim@gmail.com>
2024-05-03 16:00:49 +0300

    fix: sender

--- lab6/sender.go
+++ lab6/sender.go
@@ -2,6 +2,8 @@
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -10,15 +12,26 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	root := "."
-
+	log.Print("start send")
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() && strings.Contains(info.Name(), "exp") {
-			http.ServeFile(w, r, path)
-			filename := filepath.Base(path)
-			w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, filename))
+		if info.IsDir() && strings.Contains(info.Name(), "exp") {
+			files, err := os.ReadDir(path)
+			if err != nil {
+				return err
+			}
+			for _, file := range files {
+				if !file.IsDir() {
+					log.Print(file.Name())
+					filePath := filepath.Join(path, file.Name())
+					http.ServeFile(w, r, filePath)
+					filename := filepath.Base(filePath)
+					w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, filename))
+					io.WriteString(w, "\n")
+				}
+			}
 		}
 		return nil
 	})
@@ -29,5 +42,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	log.Print("start listen")
 	http.ListenAndServe(":1234", nil)
 }
